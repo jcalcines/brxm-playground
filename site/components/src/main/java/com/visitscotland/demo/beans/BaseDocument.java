c76360ae2a8bc820772e70cc67516c8fbc91888d
jose.calcines <jcalcines@gmail.com>
2024-02-29 15:43:32 +0000

    Initial commit

---
+++ site/components/src/main/java/com/visitscotland/demo/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package com.visitscotland.demo.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="playground:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
