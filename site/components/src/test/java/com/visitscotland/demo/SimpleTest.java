2427dd9c6e7fea770c97e0bb3d97718084277091
jose.calcines <jcalcines@gmail.com>
2024-04-02 13:29:00 +0100

    Add Junit Test

---
+++ site/components/src/test/java/com/visitscotland/demo/SimpleTest.java
@@ -0,0 +1,10 @@
+package com.visitscotland.demo;
+
+public class SimpleTest {
+
+    @Test
+    @DisplayName("Placeholder for unit tests")
+    void test(){
+        Assertions.assertTrue(true);
+    }
+}
