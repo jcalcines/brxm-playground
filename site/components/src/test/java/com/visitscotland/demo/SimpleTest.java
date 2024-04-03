8fa4b98b8ed58f913151fd1b4f11abf1981fbf5c
jose.calcines <jcalcines@gmail.com>
2024-04-03 12:43:01 +0100

    Fix Sample Unit Test

--- site/components/src/test/java/com/visitscotland/demo/SimpleTest.java
+++ site/components/src/test/java/com/visitscotland/demo/SimpleTest.java
@@ -1,5 +1,9 @@
 package com.visitscotland.demo;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class SimpleTest {
 
     @Test
